74b477625c155bb9d2c72aaf9dad41211bf63d62
odedrak <odedrak@hotmail.com>
2019-01-07 15:56:09 +0100

    Sección 12 terminada - backend terminado

--- config/config.js
+++ config/config.js
@@ -1,1 +1,4 @@
 module.exports.SEED = 'este-es-un-seed-dificil';
+
+// Google
+module.exports.CLIENT_ID = '131170900646-guf4rf2ippbls9m06ava7ilv17f7mumo.apps.googleusercontent.com';
--- models/usuario.js
+++ models/usuario.js
@@ -14,7 +14,8 @@ var usuarioSchema = new Schema({
     email: { type: String, unique: true, required: [true, 'El email es obligatorio'] },
     password: { type: String, required: [true, 'La contraseña es obligatoria'] },
     img: { type: String, required: false },
-    role: { type: String, required: false, default: 'USER_ROLE', enum: rolesValidos }
+    role: { type: String, required: false, default: 'USER_ROLE', enum: rolesValidos },
+    google: { type: Boolean, default: false }
 });
 
 usuarioSchema.plugin(uniqueValidator, { message: '{PATH} tiene que ser único' });
